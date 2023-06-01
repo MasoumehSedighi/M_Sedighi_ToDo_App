59c59e6611f9de589692119aba325e836ee89673
mas.sedighi <masoumeh.sedighii@gmail.com>
2023-06-01 23:21:10 +0330

    create tasts app

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Task(models.Model):
+    '''
+    a model for tasks
+    '''
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField()
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
